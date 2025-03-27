30fd32609e45bfd429dc5a8e588bbfed83eac67f
Om <you@example.com>
2025-03-27 14:14:51 +0530

    initial commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Overview()
+        {
+            Items item = new Items() { Name="first"};
+            return View(item);
+        }
+
+        public IActionResult Edit(int ItemId)
+        {
+            return Content($"ItemId: {ItemId}");
+        }
+
+        public IActionResult RazorData()
+        {
+            return View();
+        }
+    }
+}

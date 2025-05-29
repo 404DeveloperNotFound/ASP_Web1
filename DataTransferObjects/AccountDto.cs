e0a56da34ff073e4699d3990e1bd710106e9c969
Om <you@example.com>
2025-05-29 14:26:20 +0530

    fixes 1

--- DataTransferObjects/AccountDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebApplication1.DataTransferObjects
-{
-    public record RegisterDto(string Username, string Email, string Password, string Role);
-    public record LoginDto(string Email, string Password);
-    public record AuthenticatedUserDto(int Id, string Username, string Email, string Role);
-}
